edc8a515a8ecc12f6a4ab4ebe8391b568826a1a9
Maarten Brysbaert <maarten.brysbaert@student.howest.be>
2018-05-10 21:26:11 +0200

    video controls

--- js/video.js
+++ js/video.js
@@ -7,19 +7,24 @@
         playButton,
         videoControls,
         mute,
-        play,
         pause,
         stop,
-        muted = false;
+        muted = false,
+        paused = false,
+        muteImage,
+        soundImage,
+        playPauseImages;
 
     (function init() {
         video = document.querySelectorAll(".header_video-container_video")[0];
         nav = document.querySelectorAll(".nav")[0];
         playButton = document.querySelectorAll(".header_video-container_play-button")[0];
         videoControls = document.querySelectorAll(".header_video-controls")[0];
+        muteImage = document.querySelectorAll(".header_video-control_image-mute")[0];
+        soundImage = document.querySelectorAll(".header_video-control_image-sound")[0]
+        playPauseImages = document.querySelectorAll(".video-control-pause img");
 
         mute = document.querySelectorAll(".video-control-mute")[0];
-        play = document.querySelectorAll(".video-control-play")[0]
         pause = document.querySelectorAll(".video-control-pause")[0];
         stop = document.querySelectorAll(".video-control-stop")[0];
 
@@ -35,14 +40,25 @@
     }
 
     function videoEnded() {
-       timeVideo(34);
-       playButton.style.display = "block";
-       videoControls.style.display = "none";
-        // nav.classList.add("white-gradient");
+        timeVideo(34);
+        playPauseImages[0].style.display = "block";
+        playPauseImages[1].style.display = "none";
+        playButton.style.display = "block";
+        videoControls.style.display = "none";
     }
 
     function pauseVideo() {
-        video.pause();
+        if (paused){
+            video.play();
+            paused = false;
+            playPauseImages[0].style.display = "block";
+            playPauseImages[1].style.display = "none";
+        }else{
+            video.pause();
+            paused = true;
+            playPauseImages[1].style.display = "block";
+            playPauseImages[0].style.display = "none";
+        }
     }
 
     function stopVideo() {
@@ -58,9 +74,13 @@
         if(muted){
             video.volume = 1;
             muted = false;
+            soundImage.style.display = "none";
+            muteImage.style.display = "block";
         }else{
             video.volume = 0;
             muted = true;
+            soundImage.style.display = "block";
+            muteImage.style.display = "none";
         }
     }
 
@@ -86,12 +106,8 @@
         window.addEventListener('scroll', windowScrolled);
         playButton.addEventListener('click', videoStart);
 
-        play.addEventListener('click', playVideo);
         pause.addEventListener('click', pauseVideo);
         stop.addEventListener('click', stopVideo);
         mute.addEventListener('click', muteVideo);
-    }
-    
-    
-
+    }  
 })();
