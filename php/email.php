0c24f2e2eb08ec8fd4dc01ce26de6edd16f626d2
maarten <maarten.brysbaert@student.howest.be>
2018-05-24 16:57:27 +0200

    form

--- php/email.php
+++ php/email.php
@@ -1,21 +1,32 @@
-<?php
-$name = $_POST['firstName'];
-$last_name = $_POST['lastName'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	$name = $_POST['firstName'];
+	$last_name = $_POST['lastName'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
 
-$naam = "$name $last_name";
+	$naam = "$name $last_name";
 
-$inhoud = 
-"$message
-\n
-\n
-$name $last_name \n
-$email";
+	$inhoud = 
+	"$message
+	\n
+	\n
+	$name $last_name \n
+	$email";
 
-mail("brysbaertmaarten@hotmail.com","$naam heeft u gecontacteerd",$inhoud);
-header("index.html");
-?>
+	mail("brysbaertmaarten@hotmail.com","$naam heeft u gecontacteerd",$inhoud);
+	header("index.html");
+	?>
+</body>
+</html>
 
 
 		
